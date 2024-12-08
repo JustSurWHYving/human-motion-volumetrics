71b0cee392a7e183ac831f6bd57464e9380156e7
Ashish Prajapati <106064139+JustSurWHYving@users.noreply.github.com>
2024-12-09 04:43:33 +0530

    Added Comments

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import random
 from tracker import HumanTracker
 
 def main():
+    # Set dataset location
     video_folder = "videos/"
     video_files = [f for f in os.listdir(video_folder) if f.endswith(('.mp4', '.avi', '.mov'))]
-    
+
+    # Russian roullete with the dataset videos
     random_video = random.choice(video_files)
     random_video_path = os.path.join(video_folder, random_video)
     
     tracker = HumanTracker(random_video_path)
     tracker.process_video()
 
+# Direct execution check
 if __name__ == "__main__":
     main()
